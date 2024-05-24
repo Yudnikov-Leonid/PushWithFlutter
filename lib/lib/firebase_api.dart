3d5aed3ff4b960edfe1f98a9a82e8a1d9be9a4dd
Diryfort <dorhungmi2@gmail.com>
2024-05-24 07:25:57 +0300

    show notification when app in foreground

--- lib/lib/firebase_api.dart
+++ lib/lib/firebase_api.dart
@@ -1,15 +1,45 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:push_notifications/main.dart';
 import 'package:push_notifications/pages/notification.dart';
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  final _androidChannel = const AndroidNotificationChannel(
+      'hight_importance_channel', 'Test channel name',
+      description: 'This is a test channel for test notifications',
+      importance: Importance.defaultImportance);
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
     final fcmToken = await _firebaseMessaging.getToken();
     print('token $fcmToken');
     initPushNotifications();
+    initLocalNotifications();
+  }
+
+  Future initLocalNotifications() async {
+    const IOS = DarwinInitializationSettings();
+    const android =
+        AndroidInitializationSettings('@drawable/launch_background');
+    const settings = InitializationSettings(android: android, iOS: IOS);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (details) {
+        final message = RemoteMessage.fromMap(jsonDecode(details.payload!));
+        handleMessage(message);
+      },
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
   }
 
   Future initPushNotifications() async {
@@ -19,6 +49,21 @@ class FirebaseApi {
     _firebaseMessaging.getInitialMessage().then(handleMessage);
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+
+      _localNotifications.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+              android: AndroidNotificationDetails(
+                  _androidChannel.id, _androidChannel.name,
+                  channelDescription: _androidChannel.description,
+                  icon: '@drawable/launch_background')),
+          payload: jsonEncode(message.toMap()));
+    });
   }
 
   void handleMessage(RemoteMessage? message) {
